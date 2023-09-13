3947e8ed3c4ade2c7836ba44d9b771896c05303b
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-09-13 17:17:23 -0300

    Integration Test: Dashboard test complete

--- src/screens/Dashboard/index.test.tsx
+++ src/screens/Dashboard/index.test.tsx
@@ -1,8 +1,16 @@
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@__tests__/utils/customRender";
 import { api } from "@services/api";
 import { Dashboard } from ".";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityApiResponse";
 
 describe("Screen: Dashboard", () => {
   it("should show city weather ", async () => {
@@ -21,5 +29,41 @@ describe("Screen: Dashboard", () => {
     const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
 
     expect(cityName).toBeTruthy();
+  });
+
+  it("should show another selected weather city.", async () => {
+    const city = {
+      id: "1",
+      name: "Rio do Sul, BR",
+      latitude: 123,
+      longitude: 345,
+    };
+
+    await saveStorageCity(city);
+
+    jest
+      .spyOn(api, "get")
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+
+    render(<Dashboard />);
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
+
+    const cityName = "São Paulo";
+    await waitFor(() =>
+      act(() => {
+        const search = screen.getByTestId("search-input");
+        fireEvent.changeText(search, cityName);
+      })
+    );
+
+    await waitFor(() =>
+      act(() => {
+        fireEvent.press(screen.getByText(cityName, { exact: false }));
+      })
+    );
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy();
   });
 });
